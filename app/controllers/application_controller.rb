80b634ce969583c4c07d208d4f0823816a802830
Alex Ramos <alex.ramos@kellogg.northwestern.edu>
2024-02-21 16:00:58 +0000

    haircut break

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
 
   def index
-    render :template => "places/index"
+    #render :template => "places/index"
+    @places = Place.all
   end
+end
+
+def create
+  @place = Place.new
+  @place["name"] = params["name"]
+
+  @place.save
+  redirect_to "/places"
 end
